0be59b2ba8d216409532b1dcfe5bc095894b2dd5
Angel Angeles <daniel_1234@hotmail.es>
2023-02-07 20:46:37 -0400

    implemnting completed article view

--- client/src/Pages/ContentEditor/Article/index.tsx
+++ client/src/Pages/ContentEditor/Article/index.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Row } from "antd";
+import { Avatar, Col, List, Row, Skeleton } from "antd";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
 import { getArticleByInternalId, selectArticle } from "../../../features/article/articleSlice"
 import Paragraph from "../Paragraph";
 import { useParams } from "react-router-dom";
 import SearchKeywordsStepper from "../../../Components/App/SearchKeywordsStepper";
 import CustomLoader from "../../../Components/CustomLoader";
+import CustomButton from "../../../Components/CustomButton";
+import { EditOutlined, FileImageOutlined } from "@ant-design/icons";
 
 const ContentEditor = () => {
 
     const [open, setOpen] = useState(true)
+    const [addImageModal, openAddImageModal] = useState(true)
     let { id } = useParams();
     const article = useAppSelector(selectArticle);
 
@@ -22,15 +25,65 @@ const ContentEditor = () => {
         if(id && !open) dispatch(getArticleByInternalId(parseInt(id)))
     }, [open, id]);
 
+    //useEffect(() => {
+    //    console.log(article.status)
+    //    setOpen(!(article.article.subtitles.filter(sub => sub.content? sub.content.filter(paragraph => paragraph.selected).length === 0 : true).length === 0))
+    //}, [article]);
+
     const dispatch = useAppDispatch();
     const onNext = () => {}
+    const loading = article.status === 'loading';
 
-    if(article.status === 'loading' && article.article.subtitles.length === 0) return <CustomLoader/>
+    if(loading && article.article.subtitles.length === 0) return <CustomLoader/>
 
     return <>
+        <Row>
+            <Col style={{margin: 10}}><CustomButton onClick={() => { setOpen(true)}}>Edit Content<EditOutlined /></CustomButton></Col>
+        </Row>
         <SearchKeywordsStepper {...{onNext, open, setOpen}} subtitles={article.article.subtitles}/>
         <Row className="">
-            {article.article.subtitles.map((subtitle, index) => <Paragraph index={index} key={subtitle.id} content={subtitle}/>)}
+            <Col>
+                <List
+                    itemLayout="vertical"
+                    size="large"
+                    dataSource={article.article.subtitles}
+                    renderItem={(item) => {
+                        const contentText = item.content?.filter(paragraph => paragraph.selected).map(paragraph => <p style={{textAlign: 'start'}} key={paragraph.id}>{paragraph.content}</p>)
+                        return (
+                            <List.Item
+                                style={{textAlign: 'right'}}
+                                key={item.name}
+                                actions={
+                                !loading
+                                    ? [
+                                        <CustomButton onClick={() => { openAddImageModal(true)}}><FileImageOutlined /></CustomButton>,
+                                    ]
+                                    : undefined
+                                }
+                                extra={
+                                !loading && (
+                                    <img
+                                    width={272}
+                                    alt="logo"
+                                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                                    />
+                                )
+                                }
+                            >
+                                <Skeleton loading={loading} active avatar>
+                                    <List.Item.Meta
+                                        style={{textAlign: "left"}}
+                                        avatar={<Avatar src="https://cdn.shopify.com/s/files/1/2216/9173/files/Aceite_de_coco_sqre.jpg" />}
+                                        title={item.name}
+                                        description={`words count: ${item.content?.reduce((a, b) => a + (b.wordsCount ? b.wordsCount : 0), 0)}`}
+                                    />
+                                    {contentText}
+                                </Skeleton>
+                            </List.Item>
+                            )
+                    }}
+                />
+            </Col>
         </Row>
     </>;
 }
--- client/src/Pages/ContentEditor/Paragraph/index.tsx
+++ client/src/Pages/ContentEditor/Paragraph/index.tsx
@@ -1,8 +1,10 @@
-import { Card, Skeleton } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import { Avatar, Card, Skeleton } from "antd";
 import Meta from "antd/es/card/Meta";
 
 import React, { useEffect, useMemo, useState } from "react";
 import { useAppDispatch } from "../../../app/hooks";
+import CustomButton from "../../../Components/CustomButton";
 import { updateSubtitle } from "../../../features/article/articleSlice";
 //import { searchKeywordContent } from "../../../features/keyword/keywordAPI";
 import { SubTitleContent } from "../../../interfaces/models/Article";
@@ -11,34 +13,42 @@ import { IParagraph } from "./IParagraph";
 const Paragraph: React.FC<IParagraph> = ({ content, index }) => {
     const [ paragraphContent, setParagraphContent ] = useState<SubTitleContent>();
     const [ loading, setLoading ] = useState(true);
-    const [ contentText, setContentText ] = useState("")
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         let didCancel = false;
 
         if(content.content && content.content.find(paragraph => paragraph.selected)) setLoading(false);
-
         return () => {
             didCancel = false;
         }
     }, [content]);
 
-    useEffect(() => {
-        if(paragraphContent && paragraphContent.content)
-            setContentText(paragraphContent.content.join(". "))
-    }, [paragraphContent]);
+    const contentText = useMemo(() => content?.content?.filter(paragraph => paragraph.selected).map(paragraph => <p key={paragraph.id}>{paragraph.content}</p>),[content]);
 
     const paragraphRequireImage = useMemo(() => (index === 1 || index === 3), [index])
 
     return (<Card
         style={{ width: '100%', marginTop: 16 }}
+        actions={[
+            <CustomButton onClick={() => {}}>Edit Content<EditOutlined /></CustomButton>,
+          ]
+        }
+        extra={
+          !loading && (
+            <img
+              width={200}
+              alt="imagd"
+              src="https://cdn.shopify.com/s/files/1/2216/9173/files/Aceite_de_coco_sqre.jpg"
+            />
+          )
+        }
     >
         <Skeleton loading={loading} avatar={ paragraphRequireImage ? {shape: "square", size: 100} : false} active>
             <Meta
                 style={{textAlign: "left"}}
-                //avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                title={paragraphContent?.name}
+                avatar={<Avatar src="https://cdn.shopify.com/s/files/1/2216/9173/files/Aceite_de_coco_sqre.jpg" />}
+                title={`${content?.name.charAt(0).toUpperCase()}${content?.name.slice(1)}`}
                 description={contentText}
             />
         </Skeleton>
