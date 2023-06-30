b02d0779dc5c45a1309e64377e5f79bd6eb04990
Angel Angeles <daniel_1234@hotmail.es>
2023-06-30 07:44:32 -0400

    integrating SearchGoogleImage in image page

--- client/src/Pages/ImangesManagement/index.tsx
+++ client/src/Pages/ImangesManagement/index.tsx
@@ -8,12 +8,14 @@ import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { getImagesList, selectMedia } from '../../features/media/mediaSlice';
 import ImageGallery from '../../Components/App/ImageGallery';
 import AddImage from '../../Components/App/AddImage';
+import SearchGoogleImage from '../../Components/App/SearchGoogleImage';
 
   
   const ImagesManagement: React.FC = () => {  
     const dispatch = useAppDispatch();
     const media = useAppSelector(selectMedia);
     const [ addImageModal, openAddImageModal ] = useState(false);
+    const [ searchImageModal, openSearchImageModal ] = useState(false);
 
     useEffect(() => {
       const siteId = localStorage.getItem('default-site');
@@ -21,15 +23,11 @@ import AddImage from '../../Components/App/AddImage';
         dispatch(getImagesList(parseInt(siteId)))
     }, [dispatch])
 
-    const createNewImage = useCallback(() => {
-
-    }, [])
-
     return (
       <>
         <Row>
-            <Button style={{margin: 10}} onClick={() => {createNewImage()}}>Add Image</Button>
-            <Button style={{margin: 10}} onClick={() => {}}>Add Image By Google Search</Button>
+            <Button style={{margin: 10}} onClick={() => {openAddImageModal(true)}}>Add Image</Button>
+            <Button style={{margin: 10}} onClick={() => {openSearchImageModal(true)}}>Add Image By Google Search</Button>
             <Divider orientation="left"></Divider>
             <ImageGallery list={media.imagesList}/>
             <AddImage 
@@ -39,6 +37,13 @@ import AddImage from '../../Components/App/AddImage';
                 type={undefined} 
                 relatedId={0}
             /> 
+            <SearchGoogleImage
+                open={searchImageModal} 
+                setOpen={openSearchImageModal} 
+                title={''} 
+                type={undefined}
+                relatedId={0}
+            />
         </Row>
       </>
       
